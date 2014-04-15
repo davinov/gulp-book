b531b4a8cbecbe953a921e27fcd662862760ca21
David Nowinsky <davidn@theodo.fr>
2014-04-15 17:06:48 +0200

    gh-pages publishing script Typo Kewl reference

--- _publish.sh
+++ _publish.sh
@@ -2,8 +2,12 @@
 
 gitbook build
 MESSAGE=$(git log -1 HEAD --pretty=format:%s)
+echo $MESSAGE
 cd _book
-git add --all .
+git init
+git remote add origin git@github.com:davinov/gulp-book.git
+git checkout -b gh-pages
+git add .
 git commit -m "$MESSAGE"
-git push origin gh-pages
+git push origin gh-pages -f
 cd ..
